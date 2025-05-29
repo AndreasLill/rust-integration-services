22c551a192345f501d5012d6b7238caf5ee7dcef
Andreas Lill <dev@andreaslill.com>
2025-05-29 13:29:27 +0200

    Fixed callback parameter type.

--- src/http/http_server.rs
+++ src/http/http_server.rs
@@ -10,11 +10,11 @@ use super::http_request::HttpRequest;
 use super::http_response::HttpResponse;
 
 type RouteCallback = Arc<dyn Fn(HttpRequest) -> Pin<Box<dyn Future<Output = HttpResponse> + Send>> + Send + Sync>;
-type OnErrorCallback = Arc<dyn Fn(&String) -> Pin<Box<dyn Future<Output = ()> + Send>> + Send + Sync>;
+type OnErrorCallback = Arc<dyn Fn(String) -> Pin<Box<dyn Future<Output = ()> + Send>> + Send + Sync>;
 type OnStartCallback = Arc<dyn Fn() -> Pin<Box<dyn Future<Output = ()> + Send>> + Send + Sync>;
 type OnShutdownCallback = Arc<dyn Fn() -> Pin<Box<dyn Future<Output = ()> + Send>> + Send + Sync>;
-type OnRequestCallback = Arc<dyn Fn(&HttpRequest) -> Pin<Box<dyn Future<Output = ()> + Send>> + Send + Sync>;
-type OnResponseCallback = Arc<dyn Fn(&HttpResponse) -> Pin<Box<dyn Future<Output = ()> + Send>> + Send + Sync>;
+type OnRequestCallback = Arc<dyn Fn(HttpRequest) -> Pin<Box<dyn Future<Output = ()> + Send>> + Send + Sync>;
+type OnResponseCallback = Arc<dyn Fn(HttpResponse) -> Pin<Box<dyn Future<Output = ()> + Send>> + Send + Sync>;
 
 #[derive(Clone)]
 pub enum HttpServerControl {
@@ -64,7 +64,7 @@ impl HttpServer {
 
     pub fn on_error<T, Fut>(mut self, callback: T) -> Self
     where
-        T: Fn(&String) -> Fut + Send + Sync + 'static,
+        T: Fn(String) -> Fut + Send + Sync + 'static,
         Fut: Future<Output = ()> + Send + 'static,
     {
         self.callback_on_error = Arc::new(move |error| Box::pin(callback(error)));
@@ -91,7 +91,7 @@ impl HttpServer {
 
     pub fn on_request<T, Fut>(mut self, callback: T) -> Self
     where
-        T: Fn(&HttpRequest) -> Fut + Send + Sync + 'static,
+        T: Fn(HttpRequest) -> Fut + Send + Sync + 'static,
         Fut: Future<Output = ()> + Send + 'static,
     {
         self.callback_on_request = Arc::new(move |request| Box::pin(callback(request)));
@@ -100,7 +100,7 @@ impl HttpServer {
 
     pub fn on_response<T, Fut>(mut self, callback: T) -> Self
     where
-        T: Fn(&HttpResponse) -> Fut + Send + Sync + 'static,
+        T: Fn(HttpResponse) -> Fut + Send + Sync + 'static,
         Fut: Future<Output = ()> + Send + 'static,
     {
         self.callback_on_response = Arc::new(move |response| Box::pin(callback(response)));
@@ -146,21 +146,21 @@ impl HttpServer {
                         let request = match HttpRequest::from_stream(&mut stream).await {
                             Ok(req) => req,
                             Err(err) => {
-                                callback_on_error(&err.to_string()).await;
+                                callback_on_error(err.to_string()).await;
                                 let response = HttpResponse::internal_server_error();
                                 stream.write_all(&response.to_bytes()).await.unwrap();
-                                callback_on_response(&response).await;
+                                callback_on_response(response.clone()).await;
                                 return;
                             }
                         };
 
-                        callback_on_request(&request).await;
+                        callback_on_request(request.clone()).await;
 
                         match routes.get(&format!("{}|{}", &request.method, &request.path)) {
                             None => {
                                 let response = HttpResponse::not_found();
                                 stream.write_all(&response.to_bytes()).await.unwrap();
-                                callback_on_response(&response).await;
+                                callback_on_response(response.clone()).await;
                             },
                             Some(callback) => {
                                 let mut response = callback(request).await;
@@ -168,7 +168,7 @@ impl HttpServer {
                                     response.headers.insert(String::from("Content-Length"), response.body.len().to_string());
                                 }
                                 stream.write_all(&response.to_bytes()).await.unwrap();
-                                callback_on_response(&response).await;
+                                callback_on_response(response.clone()).await;
                             }
                         }
                     });
