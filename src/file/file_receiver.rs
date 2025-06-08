9396a5a758d9a42e442064e0c2831f70d8f319f3
Andreas Lill <dev@andreaslill.com>
2025-06-08 12:28:37 +0200

    Changed file receiver from broadcast to callbacks.

--- src/file/file_receiver.rs
+++ src/file/file_receiver.rs
@@ -1,12 +1,13 @@
 use std::{collections::HashMap, path::{Path, PathBuf}, pin::Pin, sync::Arc, time::Duration};
 
 use regex::Regex;
-use tokio::{signal::unix::{signal, SignalKind}, sync::{broadcast, Mutex}, task::JoinSet};
+use tokio::{signal::unix::{signal, SignalKind}, sync::Mutex, task::JoinSet};
 use uuid::Uuid;
 
 use crate::common::tracking_info::TrackingInfo;
 
 type FileCallback = Arc<dyn Fn(TrackingInfo, PathBuf) -> Pin<Box<dyn Future<Output = ()> + Send>> + Send + Sync>;
+type OnFileReceivedCallback = Arc<dyn Fn(TrackingInfo, PathBuf) -> Pin<Box<dyn Future<Output = ()> + Send>> + Send + Sync>;
 
 #[derive(Clone)]
 pub enum FileReceiverEventSignal {
@@ -16,18 +17,17 @@ pub enum FileReceiverEventSignal {
 pub struct FileReceiver {
     path: String,
     filter: HashMap<String, FileCallback>,
-    event_broadcast: broadcast::Sender<FileReceiverEventSignal>,
     poll_interval: u64,
+    on_file_received: OnFileReceivedCallback,
 }
 
 impl FileReceiver {
     pub fn new(path: &str) -> Self {
-        let (event_broadcast, _) = broadcast::channel(100);
         FileReceiver {
             path: path.to_string(),
             filter: HashMap::new(),
-            event_broadcast,
             poll_interval: 500,
+            on_file_received: Arc::new(|_,_| Box::pin(async {})),
         }
     }
 
@@ -46,10 +46,6 @@ impl FileReceiver {
         self
     }
 
-    pub fn get_event_broadcast(&self) -> broadcast::Receiver<FileReceiverEventSignal> {
-        self.event_broadcast.subscribe()
-    }
-
     pub async fn start(self) {
         let path = Path::new(&self.path);
         match &path.try_exists() {
@@ -58,12 +54,15 @@ impl FileReceiver {
             Err(err) => panic!("{}", err.to_string()),
         }
 
-        let ignore_list = Arc::new(Mutex::new(Vec::<String>::new()));
-        let mut join_set = JoinSet::new();
+        let mut main_join_set = JoinSet::new();
+        let mut callback_join_set = JoinSet::new();
+
         let mut interval = tokio::time::interval(Duration::from_millis(self.poll_interval));
         let mut sigterm = signal(SignalKind::terminate()).unwrap();
         let mut sigint = signal(SignalKind::interrupt()).unwrap();
-
+        let ignore_list = Arc::new(Mutex::new(Vec::<String>::new()));
+        let filter_map = Arc::new(self.filter.clone());
+        
         loop {
             tokio::select! {
                 _ = sigterm.recv() => break,
@@ -74,11 +73,11 @@ impl FileReceiver {
                         Err(err) => panic!("{}", err.to_string()),
                     };
         
-                    for (filter, callback) in self.filter.iter() {
+                    for (filter, callback) in filter_map.iter() {
                         let regex = Regex::new(filter).unwrap();
         
-                        for file in &files {
-                            let file_name = file.file_name().unwrap().to_str().unwrap().to_string();
+                        for file_path in &files {
+                            let file_name = file_path.file_name().unwrap().to_str().unwrap().to_string();
                             
                             if regex.is_match(&file_name) {
                                 let mut unlocked_list = ignore_list.lock().await;
@@ -90,14 +89,19 @@ impl FileReceiver {
                                 drop(unlocked_list);
                                 
                                 let callback = Arc::clone(&callback);
-                                let file = Arc::new(file.clone());
                                 let ignore_list = Arc::clone(&ignore_list);
-                                let tracking = TrackingInfo::new()
-                                    .uuid(Uuid::new_v4().to_string());
+                                let file_path = Arc::new(file_path.to_path_buf());
+                                let tracking = TrackingInfo::new().uuid(Uuid::new_v4().to_string());
+                            
+                                let file_path_clone = file_path.clone();
+                                let tracking_clone = tracking.clone();
+                                let on_file_received = Arc::clone(&self.on_file_received);
+                                callback_join_set.spawn(async move {
+                                    on_file_received(tracking_clone, file_path_clone.to_path_buf()).await;
+                                });
                                 
-                                self.event_broadcast.send(FileReceiverEventSignal::OnFileReceived(tracking.clone(), file.to_path_buf())).ok();
-                                join_set.spawn(async move {
-                                    callback(tracking.clone(), file.to_path_buf()).await;
+                                main_join_set.spawn(async move {
+                                    callback(tracking, file_path.to_path_buf()).await;
                                     let mut unlocked_list = ignore_list.lock().await;
                                     if let Some(pos) = unlocked_list.iter().position(|item| item == &file_name) {
                                         unlocked_list.remove(pos);
@@ -112,7 +116,8 @@ impl FileReceiver {
             }
         }
 
-        while let Some(_) = join_set.join_next().await {}
+        while let Some(_) = main_join_set.join_next().await {}
+        while let Some(_) = callback_join_set.join_next().await {}
     }
 
     async fn get_files_in_directory(path: &Path) -> tokio::io::Result<Vec<PathBuf>> {
@@ -127,5 +132,14 @@ impl FileReceiver {
         }
 
         Ok(files)
+    }
+
+    pub fn on_file_received<T, Fut>(mut self, callback: T) -> Self
+    where
+        T: Fn(TrackingInfo, PathBuf) -> Fut + Send + Sync + 'static,
+        Fut: Future<Output = ()> + Send + 'static,
+    {
+        self.on_file_received = Arc::new(move |tracking, path| Box::pin(callback(tracking, path)));
+        self
     }
 }
