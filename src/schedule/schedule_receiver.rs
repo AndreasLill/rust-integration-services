db65255dafb94c9cbe57545179faccaf475d5199
Andreas Lill <dev@andreaslill.com>
2025-06-21 22:14:26 +0200

    Changed schedule receiver to UTC exclusively to avoid daylight savings problems.

--- src/schedule/schedule_receiver.rs
+++ src/schedule/schedule_receiver.rs
@@ -1,4 +1,4 @@
-use chrono::{DateTime, Duration as ChronoDuration, Local, NaiveDate, NaiveTime, TimeZone, Utc};
+use chrono::{DateTime, Duration as ChronoDuration, NaiveDate, NaiveTime, Utc};
 use tokio::{signal::unix::{signal, SignalKind}, sync::mpsc, task::JoinSet, time::sleep};
 use uuid::Uuid;
 
@@ -10,11 +10,8 @@ pub enum ScheduleReceiverEventSignal {
 }
 
 pub struct ScheduleReceiver {
-    utc: bool,
     interval: ScheduleInterval,
     next_run: DateTime<Utc>,
-    start_date: Option<NaiveDate>,
-    start_time: Option<NaiveTime>,
     event_broadcast: mpsc::Sender<ScheduleReceiverEventSignal>,
     event_receiver: Option<mpsc::Receiver<ScheduleReceiverEventSignal>>,
     event_join_set: JoinSet<()>,
@@ -24,40 +21,37 @@ impl ScheduleReceiver {
     pub fn new() -> Self {
         let (event_broadcast, event_receiver) = mpsc::channel(128);
         ScheduleReceiver {
-            utc: false,
             interval: ScheduleInterval::None,
             next_run: Utc::now(),
-            start_date: None,
-            start_time: None,
             event_broadcast,
             event_receiver: Some(event_receiver),
             event_join_set: JoinSet::new(),
         }
     }
     
-    /// Sets the start date for the scheduled task.
+    /// Sets the `UTC` start date for the scheduled task.
     /// 
     /// If the provided date is in the past, the scheduler will calculate the next valid future run based on the defined interval.
+    /// 
+    /// Note: Scheduler is using `UTC: Coordinated Universal Time` to avoid daylight saving problems.
     pub fn start_date(mut self, year: i32, month: u32, day: u32) -> Self {
         let naive_date = NaiveDate::from_ymd_opt(year, month, day).expect("Not a valid date.");
-        self.start_date = Some(naive_date);
+        let naive_time = self.next_run.time();
+        let naive_dt = naive_date.and_time(naive_time);
+        self.next_run = DateTime::from_naive_utc_and_offset(naive_dt, Utc);
         self
     }
     
-    /// Sets the start time for the scheduled task.
+    /// Sets the `UTC` start time for the scheduled task.
     /// 
     /// If the provided time is in the past, the scheduler will calculate the next valid future run based on the defined interval.
+    /// 
+    /// Note: Scheduler is using `UTC: Coordinated Universal Time` to avoid daylight saving problems.
     pub fn start_time(mut self, hour: u32, minute: u32, second: u32) -> Self {
         let naive_time = NaiveTime::from_hms_opt(hour, minute, second).expect("Not a valid time.");
-        self.start_time = Some(naive_time);
-        self
-    }
-
-    /// Set the time zone to UTC for calculating start time and date.
-    /// 
-    /// Default is `false`, which is local timezone.
-    pub fn utc(mut self, utc: bool) -> Self {
-        self.utc = utc;
+        let naive_date = self.next_run.date_naive();
+        let naive_dt = naive_date.and_time(naive_time);
+        self.next_run = DateTime::from_naive_utc_and_offset(naive_dt, Utc);
         self
     }
     
@@ -99,8 +93,6 @@ impl ScheduleReceiver {
         let mut join_set = JoinSet::new();
         let mut sigterm = signal(SignalKind::terminate())?;
         let mut sigint = signal(SignalKind::interrupt())?;
-
-        self.next_run = Self::convert_to_utc(self.start_date, self.start_time, self.utc).await;
 
         let now_timestamp = Utc::now().timestamp();
         let next_run_timestamp = self.next_run.timestamp();
@@ -147,21 +139,6 @@ impl ScheduleReceiver {
         while let Some(_) = self.event_join_set.join_next().await {}
 
         Ok(())
-    }
-
-    async fn convert_to_utc(start_date: Option<NaiveDate>, start_time: Option<NaiveTime>, utc: bool) -> DateTime<Utc> {
-        let now = Utc::now();
-        let naive_date = start_date.unwrap_or_else(|| now.date_naive());
-        let naive_time = start_time.unwrap_or_else(|| now.time());
-        let naive_dt = naive_date.and_time(naive_time);
-
-        if utc {
-            return DateTime::<Utc>::from_naive_utc_and_offset(naive_dt, Utc)
-        }
-        
-        let offset = Local.offset_from_local_datetime(&naive_dt).single().unwrap();
-        let local_dt = offset.from_local_datetime(&naive_dt).unwrap();
-        local_dt.with_timezone(&Utc)
     }
 
     async fn calculate_next_run(next_run: DateTime<Utc>, interval: ScheduleInterval) -> DateTime<Utc> {
