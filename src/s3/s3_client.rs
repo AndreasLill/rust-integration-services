d870fe79d28249945d452a7be2f2621d11994186
Andreas Lill <dev@andreaslill.com>
2026-04-03 23:52:08 +0200

    Refactor s3 client and add support for multi part upload and stream

--- src/s3/s3_client.rs
+++ src/s3/s3_client.rs
@@ -1,19 +1,23 @@
-use std::{marker::PhantomData, path::{Path, PathBuf}, sync::Arc};
+use std::{marker::PhantomData, sync::Arc};
 
 use aws_config::{BehaviorVersion, Region, SdkConfig};
-use aws_sdk_s3::{Client, config::{Credentials, SharedCredentialsProvider}, primitives::ByteStream};
-use bytes::Bytes;
-use regex::Regex;
-use tokio::{fs::File, io::AsyncWriteExt};
+use aws_sdk_s3::{Client, config::{Credentials, SharedCredentialsProvider}, types::{CompletedMultipartUpload, CompletedPart}};
+use bytes::{Bytes, BytesMut};
+use futures::StreamExt;
+use tokio_util::io::ReaderStream;
 
-use crate::s3::s3_client_config::S3ClientConfig;
+use crate::{common::stream::ByteStream, s3::s3_client_config::S3ClientConfig};
 
 pub struct NoBucket;
 pub struct HasBucket;
 
+pub struct GetObject;
+pub struct PutObject;
+
 pub struct S3Client<State> {
     client: Arc<Client>,
     bucket: Option<String>,
+    key: Option<String>,
     _state: PhantomData<State>,
 }
 
@@ -22,6 +26,7 @@ impl S3Client<NoBucket> {
         Self {
             client: Arc::new(Self::build_client(config)),
             bucket: None,
+            key: None,
             _state: PhantomData
         }
     }
@@ -44,148 +49,32 @@ impl S3Client<NoBucket> {
         S3Client {
             client: self.client.clone(),
             bucket: Some(bucket.into()),
+            key: None,
             _state: PhantomData
         }
     }
 }
 
 impl S3Client<HasBucket> {
-    pub async fn put_object_file(&mut self, key: impl AsRef<str>, file_path: impl AsRef<Path>) -> anyhow::Result<()> {
-        let stream = ByteStream::from_path(file_path).await?;
-        let _result = self.client
-            .put_object()
-            .bucket(self.bucket.as_ref().unwrap())
-            .key(key.as_ref())
-            .body(stream)
-            .send()
-            .await?;
-
-        Ok(())
-    }
-
-    pub async fn put_object_bytes(&mut self, key: impl AsRef<str>, bytes: impl Into<Bytes>) -> anyhow::Result<()> {
-        let bytes = bytes.into();
-        let _result = self.client
-            .put_object()
-            .bucket(self.bucket.as_ref().unwrap())
-            .key(key.as_ref())
-            .body(bytes.into())
-            .send()
-            .await?;
-
-        Ok(())
-    }
-
-    pub async fn get_object(&mut self, key: impl AsRef<str>, target_path: impl AsRef<Path>) -> anyhow::Result<PathBuf> {
-        let result = self.client
-            .get_object()
-            .bucket(self.bucket.as_ref().unwrap())
-            .key(key.as_ref())
-            .send()
-            .await?;
-
-        let file_path = target_path.as_ref().join(PathBuf::from(key.as_ref()));
-        let mut file = File::create(&file_path).await?;
-        let mut stream = result.body.into_async_read();
-
-        tokio::io::copy(&mut stream, &mut file).await?;
-        file.flush().await?;
-
-        Ok(file_path)
-    }
-
-    pub async fn get_object_bytes(&mut self, key: impl AsRef<str>) -> anyhow::Result<Bytes> {
-        let result = self.client
-            .get_object()
-            .bucket(self.bucket.as_ref().unwrap())
-            .key(key.as_ref())
-            .send()
-            .await?;
-
-        let body = result.body.collect().await?;
-        let bytes = body.into_bytes();
-
-        Ok(bytes)
-    }
-
-    pub async fn get_objects(&mut self, target_path: impl AsRef<Path>) -> anyhow::Result<Vec<PathBuf>> {
-        let mut objects = Vec::new();
-
-        let mut paginator = self.client
-            .list_objects_v2()
-            .bucket(self.bucket.as_ref().unwrap())
-            .into_paginator()
-            .send();
-
-        while let Some(page) = paginator.next().await {
-            let page = page?;
-            if let Some(contents) = page.contents {
-                for obj in contents {
-                    if let Some(key) = obj.key {
-                        let result = self.client.get_object()
-                            .bucket(self.bucket.as_ref().unwrap())
-                            .key(&key)
-                            .send()
-                            .await?;
-
-                        let file_path = target_path.as_ref().join(PathBuf::from(&key));
-                        let mut file = File::create(&file_path).await?;
-                        let mut stream = result.body.into_async_read();
-
-                        tokio::io::copy(&mut stream, &mut file).await?;
-                        file.flush().await?;
-
-                        objects.push(file_path);
-                    }
-                }
-            }
+    pub fn get_object(&self, key: impl Into<String>) -> S3Client<GetObject> {
+        S3Client {
+            client: self.client.clone(),
+            bucket: self.bucket.clone(),
+            key: Some(key.into()),
+            _state: PhantomData
         }
-
-        Ok(objects)
     }
 
-    pub async fn get_objects_with_regex(&mut self, target_path: impl AsRef<Path>, regex: impl AsRef<str>) -> anyhow::Result<Vec<PathBuf>> {
-        let mut objects = Vec::new();
-        let regex = Regex::new(regex.as_ref())?;
-
-        let mut paginator = self.client
-            .list_objects_v2()
-            .bucket(self.bucket.as_ref().unwrap())
-            .into_paginator()
-            .send();
-
-        while let Some(page) = paginator.next().await {
-            let page = page?;
-            if let Some(contents) = page.contents {
-                for obj in contents {
-                    if let Some(key) = obj.key {
-                        if !regex.is_match(&key) {
-                            continue;
-                        }
-
-                        let result = self.client.get_object()
-                            .bucket(self.bucket.as_ref().unwrap())
-                            .key(&key)
-                            .send()
-                            .await?;
-
-                        let file_path = target_path.as_ref().join(PathBuf::from(&key));
-                        let mut file = File::create(&file_path).await?;
-                        let mut stream = result.body.into_async_read();
-
-                        tokio::io::copy(&mut stream, &mut file).await?;
-                        file.flush().await?;
-
-                        objects.push(file_path);
-                    }
-                }
-            }
+    pub fn put_object(&self, key: impl Into<String>) -> S3Client<PutObject> {
+        S3Client {
+            client: self.client.clone(),
+            bucket: self.bucket.clone(),
+            key: Some(key.into()),
+            _state: PhantomData
         }
-
-        Ok(objects)
     }
 
-    pub async fn delete_object(&mut self, key: impl AsRef<str>) -> anyhow::Result<()> {
+    pub async fn delete_object(&self, key: impl AsRef<str>) -> anyhow::Result<()> {
         let _result = self.client
         .delete_object()
         .bucket(self.bucket.as_ref().unwrap())
@@ -194,5 +83,129 @@ impl S3Client<HasBucket> {
         .await?;
 
         Ok(())
+    }
+}
+
+impl S3Client<GetObject> {
+    pub async fn as_bytes(&self) -> anyhow::Result<Bytes> {
+        let result = self.client
+            .get_object()
+            .bucket(self.bucket.as_ref().unwrap())
+            .key(self.key.as_ref().unwrap())
+            .send()
+            .await?;
+
+        Ok(result.body.collect().await?.into_bytes())
+    }
+
+    pub async fn as_stream(&self) -> anyhow::Result<ByteStream> {
+        let result = self.client
+            .get_object()
+            .bucket(self.bucket.as_ref().unwrap())
+            .key(self.key.as_ref().unwrap())
+            .send()
+            .await?;
+
+        let stream = ReaderStream::new(result.body.into_async_read());
+        Ok(ByteStream::new(stream))
+    }
+}
+
+impl S3Client<PutObject> {
+    pub async fn from_bytes(&self, bytes: impl Into<Bytes>) -> anyhow::Result<()> {
+        let bytes = bytes.into();
+        let _result = self.client
+            .put_object()
+            .bucket(self.bucket.as_ref().unwrap())
+            .key(self.key.as_ref().unwrap())
+            .body(bytes.into())
+            .send()
+            .await?;
+
+        Ok(())
+    }
+
+    pub async fn from_stream(&self, stream: ByteStream) -> anyhow::Result<()> {
+        let bucket = self.bucket.as_ref().unwrap();
+        let key = self.key.as_ref().unwrap();
+
+        let create_res = self.client
+            .create_multipart_upload()
+            .bucket(bucket)
+            .key(key)
+            .send()
+            .await?;
+        
+        let upload_id = create_res.upload_id().ok_or_else(|| anyhow::anyhow!("No upload ID"))?;
+        let upload_result = self.multipart_upload(upload_id, stream).await;
+
+        if let Err(err) = upload_result {
+            let _result = self.client
+            .abort_multipart_upload()
+            .bucket(bucket)
+            .key(key)
+            .upload_id(upload_id)
+            .send()
+            .await;
+
+            return Err(err);
+        }
+
+        Ok(())
+    }
+
+    async fn multipart_upload(&self, upload_id: &str, stream: ByteStream) -> anyhow::Result<()> {
+        let bucket = self.bucket.as_ref().unwrap();
+        let key = self.key.as_ref().unwrap();
+        let min_part_size: usize = 5 * 1024 * 1024;
+        let mut completed_parts = Vec::new();
+        let mut part_number = 1;
+        let mut buffer = BytesMut::with_capacity(min_part_size);
+        let mut raw_stream = stream.as_stream();
+
+        while let Some(chunk) = raw_stream.next().await {
+            let chunk = chunk?;
+            buffer.extend_from_slice(&chunk);
+
+            if buffer.len() >= min_part_size {
+                let part = self.upload_part(upload_id, part_number, buffer.split_off(0).into()).await?;
+                completed_parts.push(part);
+                part_number += 1;
+            }
+        }
+
+        if !buffer.is_empty() {
+            let part = self.upload_part(upload_id, part_number, buffer.into()).await?;
+            completed_parts.push(part);
+        }
+
+        let completed_upload = CompletedMultipartUpload::builder()
+            .set_parts(Some(completed_parts))
+            .build();
+
+        self.client
+            .complete_multipart_upload()
+            .bucket(bucket)
+            .key(key)
+            .upload_id(upload_id)
+            .multipart_upload(completed_upload)
+            .send()
+            .await?;
+
+        Ok(())
+    }
+
+    async fn upload_part(&self, upload_id: &str, part_number: i32, bytes: bytes::Bytes) -> anyhow::Result<CompletedPart> {
+        let upload_part_res = self.client
+            .upload_part()
+            .bucket(self.bucket.as_ref().unwrap())
+            .key(self.key.as_ref().unwrap())
+            .upload_id(upload_id)
+            .part_number(part_number)
+            .body(bytes.into())
+            .send()
+            .await?;
+
+        Ok(CompletedPart::builder().e_tag(upload_part_res.e_tag().unwrap_or_default()).part_number(part_number).build())
     }
 }
